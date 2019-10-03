3d5a6bc999c360c3eeefecbacbaa14a2a840ef65
sersht <sergey.shemchuk.t@gmail.com>
2019-10-03 18:31:38 +0300

    Reworked image type, now supports all basic needed operations

--- project/types/Image.py
+++ project/types/Image.py
@@ -1,28 +1,41 @@
 import base64
 from pathlib import Path
 
+
 class Image:
-    def __init__(self, imagePath, imageName='default-image', imageType='jpg'):
+    def __init__(self, inputData, isFilePath=True):
+        if isFilePath:
+            self._createFromImageFile(inputData)
+        else:
+            self._createFromBase64Code(inputData)
+
+    def _createFromImageFile(self, imagePath):
         if not Path(imagePath).is_file():
             raise ValueError('Given path is not a file')
-        
-        self._imageName = imageName
-        self._imageType = imageType
 
+        # Assume that image path file is actually an image
         with open(imagePath, 'rb') as file:
             self._imageBytesBase64 = base64.b64encode(file.read())
-    
-    def save(self, directoryPath):
-        if not Path(directoryPath).is_dir():
+
+    def _createFromBase64Code(self, data):
+        self._imageBytesBase64 = data
+
+    # Returns base64 encoded image file in string format
+    @property
+    def data(self):
+        if type(self._imageBytesBase64) is str:
+            return self._imageBytesBase64
+        else:
+            # Bytes representation is always in form of b'...'
+            # so should store it without b-character and quotes
+            bytesLiteralRepresentation = str(self._imageBytesBase64)
+            return bytesLiteralRepresentation[2:-1]
+
+    def saveOnStorage(self, directoryPath='', name='default-image', type='jpg'):
+        if directoryPath and not Path(directoryPath).is_dir():
             raise ValueError('Given path does not exist or is not a directory')
 
-        fullImagePath = directoryPath + self._imageName + '.' + self._imageType
-    
-        with open(fullImagePath, 'xb') as file:
+        fullImagePath = directoryPath + name + '.' + type
+
+        with open(fullImagePath, 'wb') as file:
             file.write(base64.b64decode(self._imageBytesBase64))
-           
-#img = Image('C:\\Projects\\information-technologies-dbms-project\\project\\t.txt', imageType='jpg')
-# with open('C:\\Projects\\information-technologies-dbms-project\\project\\t.txt', 'r') as file:
-#     a = file.read()
-#     with open('C:\\Projects\\information-technologies-dbms-project\\project\\q.jpg', 'xb') as img:
-#         img.write(base64.b64decode(a))
