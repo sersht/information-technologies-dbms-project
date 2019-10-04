bc85425f71487ffd1e812b0637fb5cf7b0fee8de
sersht <sergey.shemchuk.t@gmail.com>
2019-10-04 18:15:34 +0300

    Inited table class

---
+++ project/apps/table/table.py
@@ -0,0 +1,56 @@
+from copy import deepcopy
+
+# Temporary storing all records in table as two-dimensional list (array)
+# Maybe later will switch on dictionaries or objects if needed
+# Consequences are as follows: 
+# 1) can only "append" and "pop" (insert/delete in/from the back of table) in O(1) time-complexity
+# 2) arbitrary insertion/deletion using O(len(records)) time-complexity
+
+class Table:
+
+    def __init__(self, name, columnNames, columnTypes, database):
+        if len(columnNames) != len(columnTypes):
+            raise ValueError(
+                'Unequal length of column-type and column-name lists')
+
+        self.creatorDb = database
+        self.name = name
+        self.columns = columnNames
+        self.types = columnTypes
+        self.records = []
+
+    def _checkTypesOf(self, values):
+        for i in range(len(values)):
+            if not isinstance(value[i], self.types[i]):
+                raise ValueError(
+                    i + '-th field should be type ' + str(self.types[i]) + 
+                    ' instead of ' + str(type(value[i])))
+
+    def _checkLengthOf(self, values):
+        if len(values) != len(self.columns):
+            raise ValueError(
+                ('Add' if len(values) < len(self.columns) else 'Remove') +
+                ' some fiels to create record for ' + self.name + ' table')
+
+    # TODO: values should be list
+    # TODO: not sure, maybe replace with just .insert() - need to read docs
+    # TODO: think about better index management
+    def insert(self, values, index=None):
+        self._checkLengthOf(values)
+        self._checkTypesOf(values)
+    
+        if index is None:
+            self.records.append(deepcopy(values))
+        else:
+            self.records.insert(index, deepcopy(values))
+
+    # TODO: think about better index management
+    def delete(self, index=None):
+        if not self.records:
+            raise Exception("Can't remove record from empty table")
+        
+        if index is None:
+            self.records.pop()
+        else:
+            self.records.pop(index)
+            
